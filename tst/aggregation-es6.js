20893254da2e051899db8d9f18120f93122f5018
Ralf S. Engelschall <rse@engelschall.com>
2015-11-08 14:13:34 +0100

    upgrade dependencies and adjust source code for latest Babel world-order

--- tst/aggregation-es6.js
+++ tst/aggregation-es6.js
@@ -22,42 +22,43 @@
 **  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-/* global require: false */
+import aggregation from "../src/aggregation-es6.js"
+import { expect }  from "chai"
 
-var expect = require("chai").expect
+(function(){
+    "use strict"
 
-var aggregation = require("../src/aggregation-es6.js")
+    var Colored = class Colored {
+        initializer ()     { this._color = "white" }
+        get color ()       { return this._color }
+        set color (v)      { this._color = v }
+    }
 
-var Colored = class Colored {
-    initializer ()     { this._color = "white" }
-    get color ()       { return this._color }
-    set color (v)      { this._color = v }
-}
+    var ZCoord = class ZCoord {
+        initializer ()     { this._z = 0 }
+        get z ()           { return this._z }
+        set z (v)          { this._z = v }
+    }
 
-var ZCoord = class ZCoord {
-    initializer ()     { this._z = 0 }
-    get z ()           { return this._z }
-    set z (v)          { this._z = v }
-}
+    var Shape = class Shape {
+        constructor (x, y) { this._x = x; this._y = y }
+        get x ()           { return this._x }
+        set x (v)          { this._x = v }
+        get y ()           { return this._y }
+        set y (v)          { this._y = v }
+    }
 
-var Shape = class Shape {
-    constructor (x, y) { this._x = x; this._y = y }
-    get x ()           { return this._x }
-    set x (v)          { this._x = v }
-    get y ()           { return this._y }
-    set y (v)          { this._y = v }
-}
+    var Rectangle = class Rectangle extends aggregation(Shape, Colored, ZCoord) {}
 
-var Rectangle = class Rectangle extends aggregation(Shape, Colored, ZCoord) {}
+    var rect = new Rectangle(7, 42)
+    rect.z     = 1000
+    rect.color = "red"
 
-var rect = new Rectangle(7, 42)
-rect.z     = 1000
-rect.color = "red"
+    expect(rect.x).to.be.equal(7)
+    expect(rect.y).to.be.equal(42)
+    expect(rect.z).to.be.equal(1000)
+    expect(rect.color).to.be.equal("red")
 
-expect(rect.x).to.be.equal(7)
-expect(rect.y).to.be.equal(42)
-expect(rect.z).to.be.equal(1000)
-expect(rect.color).to.be.equal("red")
-
-console.log("OK")
+    console.log("OK")
+})()
 
